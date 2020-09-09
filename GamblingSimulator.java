9b907488fbc18bcfe9d67fa31a8a6d748624ab45
cheluvesha <cheluvesh.b@gmail.com>
2020-09-09 12:18:12 +0530

    [CHELUVESHA:ADDED] Initialization of stake and bet

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -1,1 +1,15 @@
-"Welcome to Gambling Simulator"
+//"Welcome to Gambling Simulator"
+
+class GamblingSimulator{
+        static int initialAmount=100;
+        static int betAmount=1;
+        // Welcome message
+        public static void welcomeMsg(){
+                System.out.println("Gambling will be started with $"+initialAmount+" and the Bet amount will be $"+betAmount);
+                System.out.println("<---------------------------------------------------------------------------->");
+        }
+public static void main(String args[]){
+        welcomeMsg();
+}
+}
+
