e0b4079af0f72ac1fde5740da2f60059c62b1881
cheluvesha <cheluvesh.b@gmail.com>
2020-09-09 16:57:08 +0530

    [CHELUVESHA]Refactored:Initialized and refactored variables name

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -4,13 +4,13 @@ class GamblingSimulator
 	{
 
         static int initialAmount = 100;
-	final static int betAmount = 1;
+	final static int BET_AMOUNT = 1;
         
 	// Welcome message
         public static void welcomeMsg() 
 	{
 
-                System.out.println("Gambling will be started with $"+initialAmount+" and the Bet amount will be $"+betAmount);
+                System.out.println("Gambling will be started with $"+initialAmount+" and the Bet amount will be $"+BET_AMOUNT);
                 System.out.println("<---------------------------------------------------------------------------->");
 
         }
