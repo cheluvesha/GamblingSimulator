a05fad51e880799e51ab21e12a19307c57a6008e
cheluvesha <cheluvesh.b@gmail.com>
2020-09-09 12:41:53 +0530

    [CHELUVESHA:ADDED] resign for the day if profit or loose occurred

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -2,30 +2,39 @@
 import java.util.Random;
 class GamblingSimulator{
         static int initialAmount=100;
-        static int betAmount=1;
+        final static int betAmount=1;
+        final static int stopOfWin=150;// 50% profit with additional to initial amount
+        final static int stopOfLoss=50;// 50% loss with respect to initialamount
         // Welcome message
-        public static void welcomeMsg(){
-                System.out.println("Gambling will be started with $"+initialAmount+" and the Bet amount will be $"+betAmount);
+        private static void welcomeMsg(){
+                System.out.println("Gambling will be started with $"+initialAmount+" and the bet amount will be $"+betAmount);
                 System.out.println("<---------------------------------------------------------------------------->");
-
         }
         // Random number generation
         private static int getRandom(){
-        Random random=new Random();
-        return random.nextInt(2);
+                Random random=new Random();
+                return random.nextInt(2);
         }
         // Win or Loose condition
-        private static void resultWinOrLoose(){
+        private static void resignForTheDay(){
+                while(initialAmount > stopOfLoss && initialAmount < stopOfWin){
                 int result=getRandom();
-         System.out.println(result);
-        if(result>0)
-        System.out.println("Win");
-        else
-        System.out.println("Loss");
+                if (result > 0)
+                ++initialAmount;
+                else
+                --initialAmount;
+                }
+                // Condition to check whether he has to resign for the day
+                if(initialAmount == stopOfLoss)
+                System.out.println("He Lost $"+initialAmount+ " for a day");
+                else
+                System.out.println("He Won $"+(initialAmount-100)+ " for a day");
+
         }
 
 public static void main(String args[]){
         welcomeMsg();
-        resultWinOrLoose();
+        resignForTheDay();
 }
 }
+
