b0ed84766337f1433162da8058d135c1652b2cfc
cheluvesha <cheluvesh.b@gmail.com>
2020-09-09 13:01:57 +0530

    [CHELUVESHA:ADDED] Twenty days win or lose report

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -5,6 +5,7 @@ class GamblingSimulator{
         final static int betAmount=1;
         final static int stopOfWin=150;// 50% profit with additional to initial amount
         final static int stopOfLoss=50;// 50% loss with respect to initialamount
+        static int twentyDayLoss=0,twentyDayWin=0;
         // Welcome message
         private static void welcomeMsg(){
                 System.out.println("Gambling will be started with $"+initialAmount+" and the bet amount will be $"+betAmount);
@@ -15,26 +16,45 @@ class GamblingSimulator{
                 Random random=new Random();
                 return random.nextInt(2);
         }
-        // Win or Loose condition
-        private static void resignForTheDay(){
+        // 20 days report
+        private static void getTwentyDaysReport(){
+                for( int day=1; day<=20; day++ ){
+
+                initialAmount=100;
+                // Daily report start
                 while(initialAmount > stopOfLoss && initialAmount < stopOfWin){
                 int result=getRandom();
+
                 if (result > 0)
                 ++initialAmount;
+
                 else
                 --initialAmount;
+                }// Daily report end
+                // Condition for resign
+                if(initialAmount == stopOfLoss){
+                System.out.println("He Lost $"+initialAmount+ " with the "+day+" day");
+                twentyDayLoss+=50;
                 }
-                // Condition to check whether he has to resign for the day
-                if(initialAmount == stopOfLoss)
-                System.out.println("He Lost $"+initialAmount+ " for a day");
-                else
-                System.out.println("He Won $"+(initialAmount-100)+ " for a day");
-
+                else{
+                System.out.println("He Won $"+(initialAmount-100)+ " with the "+day+" day");
+                twentyDayWin+=50;
         }
+        }
+	System.out.println("<------------------------------------------------------>");
+        // Condition to check whether its a Loss Or Win with respect to Twenty days
+        if(twentyDayLoss > twentyDayWin)
+        System.out.println("The total amount lost is $"+(twentyDayLoss-twentyDayWin));
+        else if(twentyDayLoss == twentyDayWin)
+        System.out.println("The Loss or Win is $0");
+        else
+        System.out.println("The total amount Won is $"+(twentyDayWin-twentyDayLoss));
+        System.out.println("<------------------------------------------------------>");
 
+}
 public static void main(String args[]){
         welcomeMsg();
-        resignForTheDay();
+        getTwentyDaysReport();
 }
 }
 
