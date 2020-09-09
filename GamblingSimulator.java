2e320457b0516c5ddd765565c126782bd518f019
CHELUVESHA <38749165+cheluvesha@users.noreply.github.com>
2020-09-09 21:15:00 +0530

    [CHELUVESHA]Refactored: Followed Coding Intentions

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -3,56 +3,59 @@ import java.util.Random;
 class GamblingSimulator
 	{
         
-	static int initialAmount = 100;
-        final static int BET_AMOUNT = 1;
-	static double percentage = 50.00;
-        final static int STOP_IF_WIN = initialAmount + (int)(( percentage /  100 * initialAmount ));// 50% profit with additional to initial amount
-        final static int STOP_IF_LOOSES = initialAmount - (int)(( percentage /  100 * initialAmount ));// 50% loss with respect to initialamount
+		static int initialAmount = 100;
+        	final static int BET_AMOUNT = 1;
+		static double percentage = 50.00;
+        	final static int STOP_IF_WIN = initialAmount + (int)(( percentage /  100 * initialAmount ));// 50% profit with additional to initial amount
+       		final static int STOP_IF_LOOSES = initialAmount - (int)(( percentage /  100 * initialAmount ));// 50% loss with respect to initialamount
 
-	// Welcome message
-        private static void welcomeMsg()
-	{
+		// Welcome message
+        	private static void welcomeMsg()
+		{
 
-                System.out.println("Gambling will be started with $"+initialAmount+" and the bet amount will be $"+ BET_AMOUNT);
-                System.out.println("<---------------------------------------------------------------------------->");
+               		System.out.println("Gambling will be started with $"+initialAmount+" and the bet amount will be $"+ BET_AMOUNT);
+                	System.out.println("<---------------------------------------------------------------------------->");
 
-        }
+      		 }
 
-        // Random number generation
-        private static int getRandom()
-	{
-	
-                Random random =	new Random();
-                return random.nextInt(2);
-        
-	}
-	
-        // Win or Loose condition
-        private static void resignForTheDay()	
-	{
-        
-	        while( initialAmount > STOP_IF_LOOSES && initialAmount < STOP_IF_WIN )
+       		// Random number generation
+        	private static int getRandom()
 		{
 	
-                	int gameResult = getRandom();
-                	if ( gameResult > 0 )
-              			++initialAmount;
-                	else
-                		--initialAmount;
+                	Random random =	new Random();
+                	return random.nextInt(2);
+        
+		}
+	
+        	// Win or Loose condition
+        	private static void resignForTheDay()	
+		{
+        
+	        	while( initialAmount > STOP_IF_LOOSES && initialAmount < STOP_IF_WIN )
+			{
+	
+                		int gameResult = getRandom();
+                		if ( gameResult > 0 )
+              				++initialAmount;
+                		else
+                			--initialAmount;
             
-	 	}
+	 		}
 
-                // Condition to check whether he has to resign for the day
-                if( initialAmount == STOP_IF_LOOSES )
-	                System.out.println("He Lost $"+initialAmount+ " for a day");
-                else
-        	        System.out.println("He Won $"+(initialAmount - 100)+ " for a day");
+                	// Condition to check whether he has to resign for the day
+                	if( initialAmount == STOP_IF_LOOSES )
+	                	System.out.println("He Lost $"+initialAmount+ " for a day");
+                	else
+        	        	System.out.println("He Won $"+(initialAmount - 100)+ " for a day");
 
-        }
+        		}
 
-public static void main(String args[]){
-        welcomeMsg();
-        resignForTheDay();
-}
-}
+public static void main(String args[])
+	{
+	
+        	welcomeMsg();
+        	resignForTheDay();
+	
+	}
+	}
 
