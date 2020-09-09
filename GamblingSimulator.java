6f0e4464e947ea6fd2ed27ef711eb3a935868233
cheluvesha <cheluvesh.b@gmail.com>
2020-09-09 19:24:39 +0530

    [CHELUVESHA]Refactored: followed Coding intentions

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -1,31 +1,47 @@
 //"Welcome to Gambling Simulator"
 import java.util.Random;
-class GamblingSimulator{
-        static int initialAmount=100;
-        static int betAmount=1;
-        // Welcome message
-        public static void welcomeMsg(){
-                System.out.println("Gambling will be started with $"+initialAmount+" and the Bet amount will be $"+betAmount);
-                System.out.println("<---------------------------------------------------------------------------->");
+class GamblingSimulator
+	{
+	
+		static int initialAmount = 100; 
+		final static int BET_AMOUNT = 1;
+        	
+		// Welcome message
+        	private static void welcomeMsg()
+		{
 
-        }
-        // Random number generation
-        private static int getRandom(){
-        Random random=new Random();
-        return random.nextInt(2);
-        }
-        // Win or Loose condition
-        private static void resultWinOrLoose(){
-                int result=getRandom();
-         System.out.println(result);
-        if(result>0)
-        System.out.println("Win");
-        else
-        System.out.println("Loss");
-        }
+	                System.out.println("Gambling will be started with $"+initialAmount+" and the Bet amount will be $"+BET_AMOUNT);
+        	        System.out.println("<---------------------------------------------------------------------------->");
 
-public static void main(String args[]){
-        welcomeMsg();
-        resultWinOrLoose();
-}
-}
+        	}
+        
+		// Random number generation
+        	private static int getRandom()	
+		{
+	
+        		Random random = new Random();
+        		return random.nextInt(2);
+        	
+		}
+        
+		// Win or Loose condition
+        	private static void resultWinOrLoose()
+		{
+                	
+			int gameResult = getRandom();
+        		System.out.println(result);
+        		if(gameResult > 0)
+        		System.out.println("Win");
+       			else
+        		System.out.println("Loss");
+        	
+		}
+
+public static void main(String args[])
+	{
+
+	        welcomeMsg();
+        	resultWinOrLoose();
+	
+	}
+	}
