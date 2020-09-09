a73265377d34fe072858f23fb8d9854406525009
CHELUVESHA <38749165+cheluvesha@users.noreply.github.com>
2020-09-09 21:22:02 +0530

    [CHELUVESHA]Refactored: Followed Coding Intentions

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -3,17 +3,17 @@
 class GamblingSimulator	
 	{
 
-        static int initialAmount = 100;
-	final static int BET_AMOUNT = 1;
-        
-	// Welcome message
-        public static void welcomeMsg() 
-	{
+		static int initialAmount = 100;
+		final static int BET_AMOUNT = 1;
 
-                System.out.println("Gambling will be started with $"+initialAmount+" and the Bet amount will be $"+BET_AMOUNT);
-                System.out.println("<---------------------------------------------------------------------------->");
+		// Welcome message
+		public static void welcomeMsg() 
+		{
 
-        }
+			System.out.println("Gambling will be started with $"+initialAmount+" and the Bet amount will be $"+BET_AMOUNT);
+			System.out.println("<---------------------------------------------------------------------------->");
+
+		}
 
 public static void main(String args[])
 	{
