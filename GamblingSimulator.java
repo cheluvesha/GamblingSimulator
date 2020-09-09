3a1d98fac78710b04381bd1af1ebf8edd151ca8b
cheluvesha <cheluvesh.b@gmail.com>
2020-09-09 12:31:40 +0530

    [CHELUVESHA:ADDED] Win or Loose using Random

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -1,5 +1,5 @@
 //"Welcome to Gambling Simulator"
-
+import java.util.Random;
 class GamblingSimulator{
         static int initialAmount=100;
         static int betAmount=1;
@@ -7,9 +7,25 @@ class GamblingSimulator{
         public static void welcomeMsg(){
                 System.out.println("Gambling will be started with $"+initialAmount+" and the Bet amount will be $"+betAmount);
                 System.out.println("<---------------------------------------------------------------------------->");
+
         }
+        // Random number generation
+        private static int getRandom(){
+        Random random=new Random();
+        return random.nextInt(2);
+        }
+        // Win or Loose condition
+        private static void resultWinOrLoose(){
+                int result=getRandom();
+         System.out.println(result);
+        if(result>0)
+        System.out.println("Win");
+        else
+        System.out.println("Loss");
+        }
+
 public static void main(String args[]){
         welcomeMsg();
+        resultWinOrLoose();
 }
 }
-
