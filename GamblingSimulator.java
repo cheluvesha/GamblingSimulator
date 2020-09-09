2f0e98fa4d0bcc1010457f365962ec2eb1d15fb7
cheluvesha <cheluvesh.b@gmail.com>
2020-09-09 20:10:05 +0530

    [CHELUVESHA]Refactored: Followed Coding Intentions

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -1,34 +1,52 @@
 //"Welcome to Gambling Simulator"
 import java.util.Random;
-class GamblingSimulator{
-        static int initialAmount=100;
-        final static int betAmount=1;
-        final static int stopOfWin=150;// 50% profit with additional to initial amount
-        final static int stopOfLoss=50;// 50% loss with respect to initialamount
-        // Welcome message
-        private static void welcomeMsg(){
-                System.out.println("Gambling will be started with $"+initialAmount+" and the bet amount will be $"+betAmount);
+class GamblingSimulator
+	{
+        
+	static int initialAmount = 100;
+        final static int BET_AMOUNT = 1;
+	static double percentage = 50.00;
+        final static int STOP_IF_WIN = initialAmount + (int)(( percentage /  100 * initialAmount ));// 50% profit with additional to initial amount
+        final static int STOP_IF_LOOSES = initialAmount - (int)(( percentage /  100 * initialAmount ));// 50% loss with respect to initialamount
+
+	// Welcome message
+        private static void welcomeMsg()
+	{
+
+                System.out.println("Gambling will be started with $"+initialAmount+" and the bet amount will be $"+ BET_AMOUNT);
                 System.out.println("<---------------------------------------------------------------------------->");
+
         }
+
         // Random number generation
-        private static int getRandom(){
-                Random random=new Random();
+        private static int getRandom()
+	{
+	
+                Random random =	new Random();
                 return random.nextInt(2);
-        }
+        
+	}
+	
         // Win or Loose condition
-        private static void resignForTheDay(){
-                while(initialAmount > stopOfLoss && initialAmount < stopOfWin){
-                int result=getRandom();
-                if (result > 0)
-                ++initialAmount;
-                else
-                --initialAmount;
-                }
+        private static void resignForTheDay()	
+	{
+        
+	        while( initialAmount > STOP_IF_LOOSES && initialAmount < STOP_IF_WIN )
+		{
+	
+                	int gameResult = getRandom();
+                	if ( gameResult > 0 )
+              			++initialAmount;
+                	else
+                		--initialAmount;
+            
+	 	}
+
                 // Condition to check whether he has to resign for the day
-                if(initialAmount == stopOfLoss)
-                System.out.println("He Lost $"+initialAmount+ " for a day");
+                if( initialAmount == STOP_IF_LOOSES )
+	                System.out.println("He Lost $"+initialAmount+ " for a day");
                 else
-                System.out.println("He Won $"+(initialAmount-100)+ " for a day");
+        	        System.out.println("He Won $"+(initialAmount - 100)+ " for a day");
 
         }
 
