64329f6cdf67f4f07fd1c7113585bfe65b89ba82
cheluvesha <cheluvesh.b@gmail.com>
2020-09-10 13:34:29 +0530

    [CHELUVESHA]Modified: InitialAmount variable to StakeAmount

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -4,11 +4,11 @@ import java.util.Random;
 class GamblingSimulator
 	{
         
-		static int initialAmount = 100;
+		static int stakeAmount = 100;
         	final static int BET_AMOUNT = 1;
 		static double percentage = 50.00;
-        	final static int STOP_IF_WIN = initialAmount + (int)(( percentage /  100 * initialAmount ));// 50% profit with additional to initial amount
-		final static int STOP_IF_LOOSES = initialAmount - (int)(( percentage /  100 * initialAmount ));// 50% loss with respect to initialamount
+        	final static int STOP_IF_WIN = stakeAmount + (int)(( percentage /  100 * stakeAmount ));// 50% profit with additional to initial amount
+		final static int STOP_IF_LOOSES = stakeAmount - (int)(( percentage /  100 * stakeAmount ));// 50% loss with respect to stakeAmount
         	static int twentyDayLoss = 0, twentyDayWin = 0;
         	final static int DAYS_LIMIT = 20; 
 	
@@ -16,7 +16,7 @@ class GamblingSimulator
         	private static void welcomeMsg()	
 		{
         
-	        	System.out.println("Gambling will be started with $"+initialAmount+" and the bet amount will be $"+BET_AMOUNT);
+	        	System.out.println("Gambling will be started with $"+stakeAmount+" and the bet amount will be $"+BET_AMOUNT);
                 	System.out.println("<---------------------------------------------------------------------------->");
         
 		}
@@ -36,35 +36,35 @@ class GamblingSimulator
                 	for( int day = 1; day <= DAYS_LIMIT ; day++ )
 			{
 	
-        	        	initialAmount = 100;
+        	        	stakeAmount = 100;
                
 		 		// Daily report start
-                		while( initialAmount > STOP_IF_LOOSES && initialAmount < STOP_IF_WIN )
+                		while( stakeAmount > STOP_IF_LOOSES && stakeAmount < STOP_IF_WIN )
 				{
                 
 					int gameResult = getRandom();
                 			if ( gameResult > 0 )
 						
-                				++initialAmount;
+                				++stakeAmount;
 					
 					else
 						
-                				--initialAmount;
+                				--stakeAmount;
 					
                		 	}// Daily report end
                
 		 		// Condition for resign
-                		if( initialAmount == STOP_IF_LOOSES )
+                		if( stakeAmount == STOP_IF_LOOSES )
 				{
 	
-                			System.out.println("He Lost $"+initialAmount+ " with the "+day+" day");
+                			System.out.println("He Lost $"+stakeAmount+ " with the "+day+" day");
                 			twentyDayLoss += 50;
         
 		       	 	}
                 		else
 				{
                 
-					System.out.println("He Won $"+(initialAmount - 100)+ " with the "+day+" day");
+					System.out.println("He Won $"+(stakeAmount - 100)+ " with the "+day+" day");
         	        		twentyDayWin += 50;
         	
 				}
