dc101d1ddf7744a1598ada34514ea757a3198cab
cheluvesha <cheluvesh.b@gmail.com>
2020-09-10 13:32:31 +0530

    [CHELUVESHA]Modified: InitialAmount variable to StakeAmount

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -3,17 +3,17 @@ import java.util.Random;
 class GamblingSimulator
 	{
         
-		static int initialAmount = 100;
+		static int stakeAmount = 100;
         	final static int BET_AMOUNT = 1;
 		static double percentage = 50.00;
-        	final static int STOP_IF_WIN = initialAmount + (int)(( percentage /  100 * initialAmount ));// 50% profit with additional to initial amount
-       		final static int STOP_IF_LOOSES = initialAmount - (int)(( percentage /  100 * initialAmount ));// 50% loss with respect to initialamount
+        	final static int STOP_IF_WIN = stakeAmount + (int)(( percentage /  100 * stakeAmount ));// 50% profit with additional to initial amount
+       		final static int STOP_IF_LOOSES = stakeAmount - (int)(( percentage /  100 * stakeAmount ));// 50% loss with respect to stakeAmount
 
 		// Welcome message
         	private static void welcomeMsg()
 		{
 
-               		System.out.println("Gambling will be started with $"+initialAmount+" and the bet amount will be $"+ BET_AMOUNT);
+               		System.out.println("Gambling will be started with $"+stakeAmount+" and the bet amount will be $"+ BET_AMOUNT);
                 	System.out.println("<---------------------------------------------------------------------------->");
 
       		 }
@@ -31,29 +31,29 @@ class GamblingSimulator
         	private static void resignForTheDay()	
 		{
         
-	        	while( initialAmount > STOP_IF_LOOSES && initialAmount < STOP_IF_WIN )
+	        	while( stakeAmount > STOP_IF_LOOSES && stakeAmount < STOP_IF_WIN )
 			{
 	
                 		int gameResult = getRandom();
                 		if ( gameResult > 0 )
 					
-              				++initialAmount;
+              				++stakeAmount;
 				
                 		else
 					
-                			--initialAmount;
+                			--stakeAmount;
             
 	 		}
 
                 	// Condition to check whether he has to resign for the day.
-                	if( initialAmount == STOP_IF_LOOSES )
+                	if( stakeAmount == STOP_IF_LOOSES )
 				
-	                	System.out.println("He Lost $"+initialAmount+ " for a day");
+	                	System.out.println("He Lost $"+stakeAmount+ " for a day");
 			
                 	else
 				
-        	        	System.out.println("He Won $"+(initialAmount - 100)+ " for a day");
-
+        	        	System.out.println("He Won $"+(stakeAmount - 100)+ " for a day");
+                                                                                        
         		}
 
 public static void main(String args[])
