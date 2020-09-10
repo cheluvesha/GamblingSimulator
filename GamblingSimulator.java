d4f21dbcae848df0084b5ba66b7112c7fae36464
cheluvesha <cheluvesh.b@gmail.com>
2020-09-10 13:16:55 +0530

    [CHELUVESHA]Modified: Variable name of InitialAmount to StakeAmount

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -3,14 +3,14 @@
 class GamblingSimulator	
 	{
 
-		static int initialAmount = 100;
+		static int stakeAmount = 100;
 		final static int BET_AMOUNT = 1;
 
 		// Welcome message
 		public static void welcomeMsg() 
 		{
 
-			System.out.println("Gambling will be started with $"+initialAmount+" and the Bet amount will be $"+BET_AMOUNT);
+			System.out.println("Gambling will be started with $"+stakeAmount+" and the Bet amount will be $"+BET_AMOUNT);
 			System.out.println("<---------------------------------------------------------------------------->");
 
 		}
