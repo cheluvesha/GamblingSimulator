4f5d2f5c7a08801230b097fedec0c70f0082e0de
CHELUVESHA <38749165+cheluvesha@users.noreply.github.com>
2020-09-15 15:05:34 +0530

    [CHELUVESHA]Refactored:Removed spaces and followed coding intentions.

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -3,40 +3,32 @@ import java.util.Random;
 class GamblingSimulator
 	{
         	static int stakeAmount = 100;
-        	final static int BET_AMOUNT = 1;
-        	final static int STOP_IF_WIN = 150;// 50% profit with additional to initial>
-        	final static int STOP_IF_LOOSES = 50;// 50% loss with respect to stakeAmount
+        	static final int BET_AMOUNT = 1;
+        	static final int STOP_IF_WIN = 150;// 50% profit with additional to initial>
+        	static final int STOP_IF_LOOSES = 50;// 50% loss with respect to stakeAmount
         	static int monthLoss = 0, monthWin = 0;// to store month loss and win
         	static int luckyDay = 0, unluckyDay = 0, luckyOrUnluckyAmount = 0;
         	static int maxWin = 0, maxLoss = 0;// to store maximum win and maximum loss
       		static int month = 0;
       		static int overallMonthWin = 0;
       		static int overallMonthLoss = 0;
-		final static int DAYS_LIMIT = 30;
-		
+		static final int DAYS_LIMIT = 30;
 		// Welcome message
       		private static void welcomeMsg()
-		{
-    		
+		{    		
 			System.out.println("Gambling will be started with $"+stakeAmount+" and the bet amount will be $"+BET_AMOUNT);
-      			System.out.println("<--------------------------------------------------------------------------------------->");
-      		
+      			System.out.println("<--------------------------------------------------------------------------------------->");    		
 		}
-		
 		// Random number generation
       		private static int getRandom()
-		{
-            		
+		{           		
 			Random random = new Random();
-            		return random.nextInt(2);
-     	 	
+            		return random.nextInt(2);     	 	
 		}
-		
 		// Generate Month report
       		private static void monthReport()
 		{
-
-    	  		month = 1;
+	  		month = 1;
 			// Condition to continue if current month Win rate is high against Loss
             		do
 			{
@@ -54,92 +46,67 @@ class GamblingSimulator
 		                System.out.println("MONTH NO "+month+" REPORT");
 		                System.out.println("<----------------------------------------------------------------------------------------->");
 		                for( int day = 1; day <= DAYS_LIMIT; day++ )
-				{
-                    
+				{                    
 					stakeAmount = 100;
-		    			
 					// Daily report
                     			while(stakeAmount > STOP_IF_LOOSES && stakeAmount < STOP_IF_WIN)
-					{
-                    			
+					{                 			
 						int gameResult = getRandom();
-                    				if (gameResult > 0)
-                    
+                    				if (gameResult > 0)                   
 							++stakeAmount;
-	
-        				        else
-                    	
-							--stakeAmount;
-                    				
+	       				        else                    	
+							--stakeAmount;                    				
 					}
-		    
 					// Condition to resign for the day
                     			if(stakeAmount == STOP_IF_LOOSES)
-					{
-                    				
+					{                    				
 						System.out.println("He Lost $"+stakeAmount+ " with the "+day+" day");
                     				monthLoss += 50;
                     				luckyOrUnluckyAmount -= 50;
-                    				storeAmount[day-1] = luckyOrUnluckyAmount;
-                    
+                    				storeAmount[day-1] = luckyOrUnluckyAmount;                    
 					}
                     			else
-					{
-                    
+					{                    
 						System.out.println("He Won $"+(stakeAmount-100)+ " with the "+day+" day");
                     				monthWin += 50;
                     				luckyOrUnluckyAmount += 50;
                     				storeAmount[day-1] = luckyOrUnluckyAmount;
-                    		
-					}
+       					}
                     		}
 		    		System.out.println("<------------------------------------------------------->");
                     		System.out.println("Month Loss is $"+monthLoss);
                     		System.out.println("Month Wins is $"+monthWin);
-                    		System.out.println("<------------------------------------------------------->");
-		    		
+                    		System.out.println("<------------------------------------------------------->");		    		
 				// Condition to store Overall Loss or Overall Win
                     		if(monthLoss > monthWin)
-				{
-                    
+				{                    
 					overallMonthLoss = monthLoss - monthWin;
                     			System.out.println("The total amount lost is $"+ overallMonthLoss+" so he will Quit");
                     		}                   
                     		else
 				{
-                    
-					overallMonthWin = monthWin - monthLoss;
-                    			System.out.println("The total amount Won is $"+ overallMonthWin+" so he will continue for next month");
-		    
-				}
-	
+      					overallMonthWin = monthWin - monthLoss;
+                    			System.out.println("The total amount Won is $"+ overallMonthWin+" so he will continue for next month");		    
+				}	
                     		getLuckyOrUnluckyDay(storeAmount);
         	            	month++;
-                    	}while(monthWin >= monthLoss);
-                  
-		}
-		
+                    	}while(monthWin >= monthLoss);                  
+		}		
 		 // to determine maximum win or loose amount and also the lucky or unlucky day
                 private static void getLuckyOrUnluckyDay(int arr[])
 		{
-                
-			maxWin = arr[0];
+       			maxWin = arr[0];
                         maxLoss = arr[0];
                 	for(int i = 0; i<arr.length; i++)
-			{
-	
+			{	
                             if(maxWin < arr[i])
 			    {
-                            
-				maxWin = arr[i];
-                                luckyDay = i+1;
-			    
+      				maxWin = arr[i];
+                                luckyDay = i+1;			    
 			    }
-
                             if(maxLoss > arr[i])
 			    {
-	
-                            	maxLoss = arr[i];
+				maxLoss = arr[i];
                                 unluckyDay = i+1;
                             }
                     	}
@@ -151,11 +118,9 @@ class GamblingSimulator
                     	System.out.println("The maximum loss amount which gambler got is "+maxLoss);
                   }
 
-public static void main(String args[])
-	{
-
-		welcomeMsg();
-     		monthReport();
-
+		public static void main(String args[])
+		{
+			welcomeMsg();
+     			monthReport();
 	}
-	}
+}
