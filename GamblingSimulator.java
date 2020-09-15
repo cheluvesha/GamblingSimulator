bd8b26b0fc6ce7aed4b13d95cc3020a0f0f553f7
CHELUVESHA <38749165+cheluvesha@users.noreply.github.com>
2020-09-15 14:33:46 +0530

    [CHELUVESHA]Refactored: removed Unnecessary spaces.

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -2,54 +2,42 @@
 import java.util.Random;
 class GamblingSimulator
 	{
-	
 		static int stakeAmount = 100; 
-		final static int BET_AMOUNT = 1;
+		static final int BET_AMOUNT = 1;
         	
 		// Welcome message
         	private static void welcomeMsg()
 		{
-
 	                System.out.println("Gambling will be started with $"+stakeAmount+" and the Bet amount will be $"+BET_AMOUNT);
         	        System.out.println("<---------------------------------------------------------------------------->");
-
         	}
         
 		// Random number generation
         	private static int getRandom()	
 		{
-	
         		Random random = new Random();
         		return random.nextInt(2);
-        	
 		}
         
 		// Win or Loose condition
         	private static void gameResultWinOrLoose()
 		{
-                	
 			int gameResult = getRandom();
         		if( gameResult > 0 )
 			{
-
 	        		System.out.println("Win");
 				System.out.println("The total amount will be: $"+( stakeAmount + BET_AMOUNT ));
-       	
-			}
+ 			}
 			else
         		{
-
 				System.out.println("Loss");
         			System.out.println("The total amount will be: $"+( stakeAmount - BET_AMOUNT ));
-			
 			}
 		}
 
 public static void main(String args[])
 	{
-
-	        welcomeMsg();
+		welcomeMsg();
         	gameResultWinOrLoose();
-	
 	}
 	}
