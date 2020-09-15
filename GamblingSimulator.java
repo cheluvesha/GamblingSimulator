510b5aca55d5caecca67e7f360e1d792fd422f40
CHELUVESHA <38749165+cheluvesha@users.noreply.github.com>
2020-09-15 14:31:02 +0530

    [CHELUVESHA]Refactored: Removed unnecessary spaces

--- GamblingSimulator.java
+++ GamblingSimulator.java
@@ -2,24 +2,18 @@
 
 class GamblingSimulator	
 	{
-
 		static int stakeAmount = 100;
-		final static int BET_AMOUNT = 1;
-
+		static final int BET_AMOUNT = 1;
 		// Welcome message
 		public static void welcomeMsg() 
 		{
-
 			System.out.println("Gambling will be started with $"+stakeAmount+" and the Bet amount will be $"+BET_AMOUNT);
 			System.out.println("<---------------------------------------------------------------------------->");
-
 		}
 
 public static void main(String args[])
 	{
-
         	welcomeMsg();
-
 	}
 	}
 
